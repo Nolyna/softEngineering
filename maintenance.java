57889dcf3e43d3bb67aa82e05e7fd72a65d7949e
cwright2733 <32071495+cwright2733@users.noreply.github.com>
2017-11-14 02:24:02 -0500

    Add files via upload

---
+++ maintenance.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HSMmodel;
+
+/**
+ *
+ * @author cameron wright
+ */
+public class maintenance {
+    
+    private String name, time, room;
+    
+    public maintenance(){}
+    
+    
+    public void setName(String n){
+        this.name = n;
+    }
+    public void setTime(String t){
+        this.time = t ;
+    }
+    public void setroom(String r){
+        
+    }
+    
+
+  
+        
+    
+    
+}
